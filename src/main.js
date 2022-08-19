0409306ed04f11d86538162edc6e71ce29d8b8c6
RojaVictoria <82482780+RojaVictoria@users.noreply.github.com>
2022-08-19 00:54:21 -0400

    json update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import firebase from "./plugins/firebase"
+import firebase from "./plugins/firebase";
 
 
 Vue.config.productionTip = false;
